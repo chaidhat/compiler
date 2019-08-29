643dda0e1337a46f011d4bc0621bdae3d729bf50
Chai112 <chaidhatchaimongkol@Chaidhat-Chaimongkol-2.local>
2019-08-29 17:15:30 +0700

    BCC can now read & store files

--- main.c
+++ main.c
@@ -2,32 +2,38 @@
 #include <stdlib.h>
 
 char* dSuf = "Bit-C Compiler: ";
-char* data[128];
+char data[128];
 FILE* fp;
-char* ch;
+char ch;
 
 // reads files
 void IOread (char *filename)
 {
     fp = fopen(filename, "r"); // read mode
 
-   if (fp == NULL)
-   {
-      perror("Error while opening the file.\n");
-      exit(EXIT_FAILURE);
-   }
+    if (fp == NULL)
+    {
+        perror("Error while opening the file.\n");
+        abort();
+    }
 
-   printf("The contents of %s file are:\n", filename);
+    printf("The contents of %s file are:\n", filename);
 
-   while((ch = fgetc(fp)) != EOF)
-      printf("%c", ch);
+    int i = 0;
+    while((ch = fgetc(fp)) != EOF)
+    {
+        printf("%c", ch);
+        data[i] = ch;
+        i++;
+    }
 
-   fclose(fp);
+    fclose(fp);
 }
 
 int main (int argc, char* argv[])
 {
     printf("%sChaidhat Chaimongkol\n", dSuf);
     IOread("/Users/chaidhatchaimongkol/Documents/GitHub/Bit-C-Compiler/README.md");  
+    printf("%c", data[2]);
     printf("%sTerminated\n", dSuf);
 }
